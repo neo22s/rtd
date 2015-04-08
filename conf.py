b82bf787927546945f9df763f2e7abbed22c21ce
ConstantinosOC <constantinos@open-classifieds.com>
2015-04-08 19:18:35 +0300

    remove labibi.css

--- conf.py
+++ conf.py
@@ -283,5 +283,4 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 
-
-html_style = 'labibi.css'
+#html_style = 'labibi.css'
